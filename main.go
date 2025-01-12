66cde0f1d5fa50463275d2fdeb8e4911c33c7365
Andrei Lebedev <lebdron@gmail.com>
2025-01-12 13:21:51 +0000

    Update avalanche to v1.12.1

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/x509"
 	"encoding/hex"
@@ -18,25 +17,28 @@ import (
 	"strings"
 	"time"
 
+	"filippo.io/keygen"
 	"github.com/ava-labs/avalanche-network-runner/network"
 	"github.com/ava-labs/avalanche-network-runner/utils"
 	"github.com/ava-labs/avalanchego/genesis"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/constants"
+	blst "github.com/supranational/blst/bindings/go"
 )
 
 type Config struct {
-	NetworkId                 string `json:"network-id"`
-	PublicIp                  string `json:"public-ip"`
-	StakingPort               uint   `json:"staking-port"`
-	HttpHost                  string `json:"http-host"`
-	HttpPort                  uint   `json:"http-port"`
-	StakingTlsCertFileContent []byte `json:"staking-tls-cert-file-content"`
-	StakingTlsKeyFileContent  []byte `json:"staking-tls-key-file-content"`
-	GenesisFileContent        []byte `json:"genesis-file-content"`
-	BootstrapIds              string `json:"bootstrap-ids"`
-	BootstrapIps              string `json:"bootstrap-ips"`
-	LogLevel                  string `json:"log-level,omitempty"`
+	NetworkId                   string `json:"network-id"`
+	PublicIp                    string `json:"public-ip"`
+	StakingPort                 uint   `json:"staking-port"`
+	HttpHost                    string `json:"http-host"`
+	HttpPort                    uint   `json:"http-port"`
+	StakingTlsCertFileContent   []byte `json:"staking-tls-cert-file-content"`
+	StakingTlsKeyFileContent    []byte `json:"staking-tls-key-file-content"`
+	StakingSignerKeyFileContent []byte `json:"staking-signer-key-file-content"`
+	GenesisFileContent          []byte `json:"genesis-file-content"`
+	BootstrapIds                string `json:"bootstrap-ids"`
+	BootstrapIps                string `json:"bootstrap-ips"`
+	LogLevel                    string `json:"log-level,omitempty"`
 }
 
 type CConfig struct {
@@ -45,20 +47,21 @@ type CConfig struct {
 
 func NewCertAndKeyBytes(rand io.Reader) ([]byte, []byte, error) {
 	// Create key to sign cert with
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand)
+	key, err := keygen.ECDSALegacy(elliptic.P256(), rand)
 	if err != nil {
 		return nil, nil, fmt.Errorf("couldn't generate ed25519 key: %w", err)
 	}
 
 	// Create self-signed staking cert
+	date := time.Date(2000, time.January, 0, 0, 0, 0, 0, time.UTC)
 	certTemplate := &x509.Certificate{
 		SerialNumber:          big.NewInt(0),
-		NotBefore:             time.Date(2000, time.January, 0, 0, 0, 0, 0, time.UTC),
-		NotAfter:              time.Now().AddDate(100, 0, 0),
+		NotBefore:             date,
+		NotAfter:              date.AddDate(100, 0, 0),
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageDataEncipherment,
 		BasicConstraintsValid: true,
 	}
-	certBytes, err := x509.CreateCertificate(rand, certTemplate, certTemplate, &key.PublicKey, key)
+	certBytes, err := x509.CreateCertificate(nil, certTemplate, certTemplate, &key.PublicKey, key)
 	if err != nil {
 		return nil, nil, fmt.Errorf("couldn't create certificate: %w", err)
 	}
@@ -107,6 +110,7 @@ func main() {
 	}
 
 	rand := rand.New(rand.NewSource(0))
+	nodesKeys := make([]*utils.NodeKeys, 0)
 	genesisVdrs := make([]ids.NodeID, 0)
 	scanner := bufio.NewScanner(nodefile)
 	for scanner.Scan() {
@@ -116,6 +120,18 @@ func main() {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		var ikm [32]byte
+		_, err = rand.Read(ikm[:])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		sk := blst.KeyGen(ikm[:])
+		nodesKeys = append(nodesKeys, &utils.NodeKeys{
+			StakingKey:  key,
+			StakingCert: crt,
+			BlsKey:      sk.Serialize(),
+		})
 		nodeID, err := utils.ToNodeID(key, crt)
 		if err != nil {
 			fmt.Println(err)
@@ -141,6 +157,7 @@ func main() {
 		config.StakingPort = uint(stakingPort)
 		config.StakingTlsCertFileContent = crt
 		config.StakingTlsKeyFileContent = key
+		config.StakingSignerKeyFileContent = sk.Serialize()
 		if len(genesisVdrs) == 1 {
 			baseConfig.BootstrapIds = nodeID.String()
 			baseConfig.BootstrapIps = fmt.Sprintf("%s:%s", tokens[0], tokens[2])
@@ -173,40 +190,37 @@ func main() {
 		})
 	}
 
-	genesisBytes, err := network.NewAvalancheGoGenesis(
-		networkID,
-		nil,
-		cChainBalances,
-		genesisVdrs,
-	)
+	genesisBytes, err := utils.GenerateGenesis(networkID, nodesKeys)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Enable upgrades
 	var genesisConfig genesis.UnparsedConfig
 	err = json.Unmarshal(genesisBytes, &genesisConfig)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	// startTime := time.Date(2025, time.January, 0, 0, 0, 0, 0, time.UTC).Unix()
+	// genesisConfig.StartTime = uint64(startTime)
 	var cChainConfig map[string]interface{}
 	err = json.Unmarshal([]byte(genesisConfig.CChainGenesis), &cChainConfig)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	blockConfig := cChainConfig["config"].(map[string]interface{})
-	blockConfig["apricotPhase3BlockTimestamp"] = 0
-	blockConfig["apricotPhase4BlockTimestamp"] = 0
-	blockConfig["apricotPhase5BlockTimestamp"] = 0
-	blockConfig["apricotPhasePre6BlockTimestamp"] = 0
-	blockConfig["apricotPhase6BlockTimestamp"] = 0
-	blockConfig["apricotPhasePost6BlockTimestamp"] = 0
-	blockConfig["banffBlockTimestamp"] = 0
-	blockConfig["cortinaBlockTimestamp"] = 0
-	cChainConfig["config"] = blockConfig
+
+	cChainAllocs := map[string]interface{}{}
+	for _, cChainBal := range cChainBalances {
+		addrHex := fmt.Sprintf("0x%s", cChainBal.Addr.Hex())
+		balHex := fmt.Sprintf("0x%x", cChainBal.Balance)
+		cChainAllocs[addrHex] = map[string]interface{}{
+			"balance": balHex,
+		}
+	}
+	cChainConfig["alloc"] = cChainAllocs
+
 	cChainConfigBytes, err := json.Marshal(cChainConfig)
 	if err != nil {
 		fmt.Println(err)
